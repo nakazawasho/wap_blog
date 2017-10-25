fbd043ef2f974071d69c1486211215c6b58591b5
nakazawasho <syodobsyo@gmail.com>
2017-10-25 16:30:48 +0900

    http認証の完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :basic_authentication
+
+  def basic_authentication
+      authenticate_or_request_with_http_basic do |user, pass|
+        user == ENV['PASS'] && pass == ENV['PASS']
+      end
+  end
 end
